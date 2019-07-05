6b58d44fd187221d1bfd2992e80ac2c9d9f5f1ab
wangaiguo <aigo@mail.com>
2019-07-05 22:30:23 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # libcurl_android_ndk
+
+Origin Text:  
+https://blog.csdn.net/u012787710/article/details/53870348
---
+++ curl-7.52.1/jni/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH:= $(call my-dir)
+
+CFLAGS := -Wpointer-arith -Wwrite-strings -Wunused -Winline \
+ -Wnested-externs -Wmissing-declarations -Wmissing-prototypes -Wno-long-long \
+ -Wfloat-equal -Wno-multichar -Wsign-compare -Wno-format-nonliteral \
+ -Wendif-labels -Wstrict-prototypes -Wdeclaration-after-statement \
+ -Wno-system-headers -DHAVE_CONFIG_H -DBUILDING_LIBCURL
+
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/curl/lib/Makefile.inc
+
+#CSOURCES 在 Makefile.inc中定义
+LOCAL_SRC_FILES := $(addprefix curl/lib/,$(CSOURCES))
+LOCAL_CFLAGS += $(CFLAGS)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/curl/include/ $(LOCAL_PATH)/curl/lib
+
+LOCAL_COPY_HEADERS_TO := libcurl
+LOCAL_COPY_HEADERS := $(addprefix curl/include/curl/,$(HHEADERS))
+
+LOCAL_MODULE:= libcurl
+
+#include $(BUILD_STATIC_LIBRARY)
+
+# Build shared library now
+include $(BUILD_SHARED_LIBRARY)
+
